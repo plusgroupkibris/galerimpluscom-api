5e6f7d54d4abc6d8aad0d9f7cf11b591b67bd2b2
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2025-08-14 18:15:10 +0300

    car lsitn modeli eklendi

---
+++ GalerimPlusAPI/Context/MongoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GalerimPlusAPI.Models;
+using MongoDB.Driver;
+
+namespace GalerimPlusAPI.Context;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["MongoDbSettings:ConnectionString"]);
+        _database = client.GetDatabase(configuration["MongoDbSettings:DatabaseName"]);
+    }
+
+    public IMongoCollection<CarListing> CarListings => _database.GetCollection<CarListing>("CarListings");
+
+
+
+}
---
+++ GalerimPlusAPI/Enums/ListingStatus.cs
@@ -0,0 +1,26 @@
+﻿namespace GalerimPlusAPI.Enums;
+
+public enum ListingStatus
+{
+    Active,
+    Inactive,
+    Draft,
+    Pending,
+    AwaitingApproval,
+    Sold,
+    Expired,
+    Rejected,
+    Removed
+}
+
+public enum ListingType
+{
+    Sale,
+    Rent,
+    Auction,
+    Lease,
+    Exchange,
+    Service,
+    DailyRent,
+    Other
+}
