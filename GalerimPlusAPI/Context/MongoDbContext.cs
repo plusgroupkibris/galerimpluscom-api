30677af0fd0feb55de9db5c1a74ba7cd444b30a8
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2025-08-16 14:00:57 +0300

    ddd

--- GalerimPlusAPI/Context/MongoDbContext.cs
+++ GalerimPlusAPI/Context/MongoDbContext.cs
@@ -1,20 +1,7 @@
-﻿using GalerimPlusAPI.Models;
-using MongoDB.Driver;
-
-namespace GalerimPlusAPI.Context;
+﻿namespace GalerimPlusAPI.Context;
 
 public class MongoDbContext
 {
-    private readonly IMongoDatabase _database;
-
-    public MongoDbContext(IConfiguration configuration)
-    {
-        var client = new MongoClient(configuration["MongoDbSettings:ConnectionString"]);
-        _database = client.GetDatabase(configuration["MongoDbSettings:DatabaseName"]);
-    }
-
-    public IMongoCollection<CarListing> CarListings => _database.GetCollection<CarListing>("CarListings");
-
-
+    
 
 }
